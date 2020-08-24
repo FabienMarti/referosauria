9bf4ee3bf3243f6a5bc5ee484c0439018ce674c9
FabienMarti <martiretti.fabien@gmail.com>
2020-08-24 21:41:51 +0200

    Ajout bootstrap et controller page ajout creature

--- controllers/addCreatureController.php
+++ controllers/addCreatureController.php
@@ -1,37 +1,117 @@
 <?php
-$regexName = '%^[A-ÿ0-9_\-]{2,30}$%';
+//Regex nom
+$regexName = '%^[A-ÿ_\-\ ]{2,30}$%';
+
+//Tableau des extensions de fichier autorisées
 $extensionsWhiteList = array('jpg', 'png');
 
+//Tableau d'erreurs
 $formErrors = array();
 
+//Tableaux menu déroulants
+$dinoPeriod = array('Trias inférieur', 'Trias moyen', 'Trias supérieur', 'Jurassique inférieur', 'Jurassique moyen', 'Jurassique supérieur', 'Crétacé inférieur', 'Crétacé supérieur');
+$dinoType = array('Carnivore', 'Herbivore', 'Piscivore');
+$environmentArray = array('Amérique du Nord', 'Amérique du Sud', 'Europe', 'Asie', 'Afrique', 'Océanie', 'Antartique' );
+$categories = array('Dinosaure', 'Mammifère', 'Réptile marin', 'Réptile Volant', 'Autre');
+
 if(isset($_POST['sendNewCrea'])){
-    if(!empty($_POST)){
+ 
 
-        //controle de la radio typeSelect
+        //Contrôle de la radio typeSelect
+        if(!empty($_POST['typeSelect'])) {
+            if(in_array($_POST['typeSelect'], $categories)) {
+                $typeSelect = htmlspecialchars($_POST['typeSelect']);               
+            }else {
+                $formErrors['typeSelect'] = 'Merci de sélectionner un type de créature';
+            }
+        }else {
+            $formErrors['typeSelect'] = 'Veuillez sélectionner un type de créature';
+        }
 
-
-        //controle du Nom de la creature
+        //Contrôle du Nom de la creature
         if(isset($_POST['creaName'])){
             if(preg_match($regexName, $_POST['creaName'])){
                 $creaName = htmlspecialchars($_POST['creaName']);
             }else{
-                $formErrors['creaName'] = 'Format Incorrect, commencez par une majuscule';
+                $formErrors['creaName'] = 'Format Incorrect, 2 lettres minimum, aucun chiffre';
             }
         }else{
             $formErrors['creaName'] = 'Veuillez renseigner un nom pour la créature';
         }
 
-        //Ajout Fichier
-        if (isset($_FILES['imageUpload'])) {
+        //Contrôle de l'ajout de fichier
+        if(isset($_FILES['imageUpload'])) {
             $extensionUpload = pathinfo($_FILES['imageUpload']['name'], PATHINFO_EXTENSION); // recupere le nom du fichier
             //test si l'élement extention upload se trouve dans le tableau extention autorisées
-            if (in_array(strtolower($extensionUpload), $extensionsWhiteList)){
-                $imageUpload =  'Vous avez envoyer ' . $_FILES['imageUpload']['name'] . ' Il s\'agit d\'un fichier .' . $extensionUpload;
-            }else {
-                $formError['imageUpload'] = 'Les formats acceptés sont : ' . implode(', ', $extensionsWhiteList);
+            if(in_array(strtolower($extensionUpload), $extensionsWhiteList)){
+                $imageUpload =  'Vous avez envoyé ' . $_FILES['imageUpload']['name'] . ' Il s\'agit d\'un fichier .' . $extensionUpload;
+            }else{
+                $formErrors['imageUpload'] = 'Les formats acceptés sont : ' . implode(', ', $extensionsWhiteList);
             }
-        }else {
-            $formError['imageUpload'] = 'Veuillez sélectioner un fichier';
+        }else{
+            $formErrors['imageUpload'] = 'Veuillez sélectionner un fichier';
+        }
+
+        ##### Contrôle des menus déroulants #####
+
+        //Contrôle de la période
+        if(!empty($_REQUEST['period'])) {
+            if(in_array($_REQUEST['period'], $dinoPeriod)) {
+                $period = htmlspecialchars($_REQUEST['period']);
+            }else{
+                $formErrors['period'] = 'Une erreur est survenue';
+            }
+        }else{
+            $formErrors['period'] = 'Veuillez sélectionner une période';
+        }
+
+        //Contrôle de l'habitat
+        if(!empty($_REQUEST['habitat'])) {
+            if(in_array($_REQUEST['habitat'], $environmentArray)) {
+                $habitat = htmlspecialchars($_REQUEST['habitat']);
+            }else{
+                $formErrors['habitat'] = 'Une erreur est survenue';
+            }
+        }else{
+            $formErrors['habitat'] = 'Veuillez sélectionner un habitat';
+        }
+
+        //Contrôle alimentation
+        if(!empty($_REQUEST['diet'])) {
+            if(in_array($_REQUEST['diet'], $dinoType)) {
+                $diet = htmlspecialchars($_REQUEST['diet']);
+            }else{
+                $formErrors['diet'] = 'Une erreur est survenue';
+            }
+        }else{
+            $formErrors['diet'] = 'Veuillez sélectionner une alimentation';
+        }
+
+        ##### FIN GESTION MENUS DEROULANTS #####
+
+        //Contrôle Paléonthologue
+        if(isset($_POST['discoverer'])){
+            if(preg_match($regexName, $_POST['discoverer'])){
+                //Met la chaine de caracteres en minuscules
+                $lowerCaseName = strtolower(htmlspecialchars($_POST['discoverer']));
+                //Passe la 1ere lettre de chaque mot en Majuscule
+                $discoverer = ucwords($lowerCaseName, ' ');
+            }else{
+                $formErrors['discoverer'] = 'Format Incorrect, 2 lettres minimum, aucun chiffre';
+            }
+        }else{
+            $formErrors['discoverer'] = 'Veuillez renseigner un nom pour la créature';
+        }
+
+        //Contrôle de la description #####BUGGEE SUR LA VERIF DE LONGUEUR#######
+        if(isset($_POST['description'])){
+            if(strlen($_POST['description']) <= 5){
+                $formErrors['description'] = 'Description trop courte, 500 caractères minimum (espaces inclus)';
+            }else{
+                $description = htmlspecialchars($_POST['description']);
+            }
+        }else{
+            $formErrors['description'] = 'Veuillez renseigner une description';
         }
 
 
@@ -63,10 +143,7 @@ if(isset($_POST['sendNewCrea'])){
 
 
 
-
-
-
-    }
+    
 
 
 
--- views/addCreature.php
+++ views/addCreature.php
@@ -2,73 +2,97 @@
 $pageTitle = 'Ajout créature';
 include 'parts/header.php';
 include '../controllers/addCreatureController.php';
-$dinoPeriod = array('1' => 'Trias inférieur','2' => 'Trias moyen', '3' => 'Trias supérieur', '4' => 'Jurassique inférieur', '5' => 'Jurassique moyen', '6' => 'Jurassique supérieur', '7' => 'Crétacé inférieur', '8' => 'Crétacé supérieur');
-$dinoType = array('Carnivore', 'Herbivore');
-$environmentArray = array('USA', 'europe');
-$categories = array('Dinosaure', 'Mammifère', 'Réptile marin', 'Réptile Volant', 'Autre');
 include '../controllers/breadcrumb.php';
 generateBreadcrumb(array('../index.php' => 'Referosauria', 'final' => $pageTitle));
 ?>
-<div class="container border border-dark rounded p-5 mt-5">
-    <form action="" method="post">
-        
-            <!-- Selection type de catégorie -->
-            <div class="form-group d-flex justify-content-around">
+<div class="container border border-dark rounded pt-5 px-5 my-5">
+    <form action="" method="POST" enctype="multipart/form-data">
+<!-- Selection type de catégorie -->
+            <p>Sélectionnez un type de créature : </p>
+            <div class="form-group d-flex justify-content-between">
                 <?php 
                     foreach ($categories as $category) { ?>
-                        <label for="<? $category ?>"><?= $category ?> : </label>
-                        <input type="radio" name="typeSelect" id="<?= $category ?>" value="<?= $category ?>" />
-                   <?php } ?>
+                        <label class="my-auto" for="<? $category ?>"><?= $category ?> : </label>
+                        <input type="radio" name="typeSelect" id="<?= $category ?>" value="<?= $category ?>" <?= isset($_POST['typeSelect']) && $_POST['typeSelect'] == $category ? 'checked' : '' ?> class="my-auto <?= isset($formErrors['typeSelect']) ? 'is-invalid' : (isset($typeSelect) ? 'is-valid' : '') ?>" />
+                   <?php } 
+                   if (isset($formErrors['typeSelect'])) { ?>
+                        <div class="text-danger d-block"><?= $formErrors['typeSelect'] ?></div>
+                    <?php } ?>
             </div>
-            <!-- Nom de la créature -->
+<!-- Nom de la créature -->
             <div class="row">
-                <div class="form-group col-md-8">
+                <div class="form-group col-md-8 <?= count($_POST) > 0 ? (isset($formErrors['creaName']) ? 'has-danger' : 'has-success') : '' ?>">
                     <label for="creaName">Nom : </label>
-                    <input type="text" class="form-control" id="creaName" name="creaName" placeholder="Nom de la Créature" />
+                    <input type="text" id="creaName" name="creaName" placeholder="Nom de la Créature" class="form-control <?= count($_POST) > 0 ? (isset($formErrors['creaName']) ? 'is-invalid' : 'is-valid') : '' ?>" <?= isset($_POST['creaName']) ? 'value="' . $_POST['creaName'] . '"' : '' ?> />
+            <?php if (isset($formErrors['creaName'])) { ?>
+                <p class="text-danger text-center"><?= $formErrors['creaName'] ?></p>
+            <?php } ?>
                 </div>
-                <!-- Image principale de la créature -->
-                <div class="form-group col-md-4">
+<!-- Image principale de la créature FILE -->
+                <div class="form-group col-md-4 <?= count($_POST) > 0 ? (isset($formErrors['imageUpload']) ? 'has-danger' : 'has-success') : '' ?>">
                     <label for="imageUpload">Image de la créature : </label>
-                    <input type="file" class="form-control-file" name="imageUpload" id="imageUpload" />
+                    <input type="file" name="imageUpload" id="imageUpload" class="form-control-file <?= count($_POST) > 0 ? (isset($formErrors['imageUpload']) ? 'is-invalid' : 'is-valid') : '' ?>" />
+            <?php if (isset($formErrors['imageUpload'])) { ?>
+                <p class="text-danger text-center"><?= $formErrors['imageUpload'] ?></p>
+            <?php } ?>
                 </div> 
             </div>
+<!-- Menus déroulants #### Période #### Habitat #### Alimentation #### Découverte -->
         <div class="row justify-content-between">
-            <div class="form-group col">
+<!-- Menu Période -->
+            <div class="form-group col <?= count($_POST) > 0 ? (isset($formErrors['period']) ? 'has-danger' : 'has-success') : '' ?>">
                 <label for="period">Choisissez une période :</label>
-                <select class="form-control" name="period">
-                    <option value="" disabled selected>Selectionnez</option>
+                <select name="period" class="form-control <?= count($_POST) > 0 ? (isset($formErrors['period']) ? 'is-invalid' : 'is-valid') : '' ?>" <?= isset($_POST['period']) ? 'value="' . $_POST['period'] . '"' : '' ?>>
+                    <option value="" disabled selected>Sélectionnez</option>
                     <?php
-                        foreach ($dinoPeriod as $id => $period) {
-                    ?><option value="<?= $id ?>"><?= $period ?></option><?php } ?>
+                        foreach ($dinoPeriod as $period) {
+                    ?><option value="<?= $period ?>" <?= isset($_POST['period']) ? ($_POST['period'] == $period ? 'selected' : '') : '' ?>><?= $period ?></option><?php } ?>
                 </select>
+                <?php if (isset($formErrors['period'])) { ?>
+                        <p class="text-danger text-center"><?= $formErrors['period'] ?></p>
+                <?php } ?>
             </div>
-            <div class="form-group col">
-                <label for="period">Habitat principal :</label>
-                <select class="form-control" name="period">
-                    <option value="" disabled selected>Selectionnez</option>
+<!-- Menu Habitat -->
+            <div class="form-group col <?= count($_POST) > 0 ? (isset($formErrors['habitat']) ? 'has-danger' : 'has-success') : '' ?>">
+                <label for="habitat">Habitat principal :</label>
+                <select name="habitat"  class="form-control <?= count($_POST) > 0 ? (isset($formErrors['habitat']) ? 'is-invalid' : 'is-valid') : '' ?>" <?= isset($_POST['habitat']) ? 'value="' . $_POST['habitat'] . '"' : '' ?>>
+                    <option value="" disabled selected>Sélectionnez</option>
                     <?php
-                    foreach ($environmentArray as $area) {
-                    ?><option value="<?= $area ?>"><?= $area ?></option><?php } ?>
+                        foreach ($environmentArray as $area) {
+                    ?><option value="<?= $area ?>" <?= isset($_POST['habitat']) ? ($_POST['habitat'] == $area ? 'selected' : '') : '' ?>><?= $area ?></option><?php } ?>
                 </select>
+                <?php if (isset($formErrors['habitat'])) { ?>
+                        <p class="text-danger text-center"><?= $formErrors['habitat'] ?></p>
+                <?php } ?>
             </div>
-            <div class="form-group col">
+<!-- Menu Alimentation -->
+            <div class="form-group col <?= count($_POST) > 0 ? (isset($formErrors['diet']) ? 'has-danger' : 'has-success') : '' ?>">
                 <label for="diet">Choisissez l'alimentation : </label>
-                <select class="form-control" name="diet">
-                    <option value="" disabled selected>Selectionnez</option>
+                <select name="diet"  class="form-control <?= count($_POST) > 0 ? (isset($formErrors['diet']) ? 'is-invalid' : 'is-valid') : '' ?>" <?= isset($_POST['diet']) ? 'value="' . $_POST['diet'] . '"' : '' ?>>
+                    <option value="" disabled selected>Sélectionnez</option>
                     <?php
-                    foreach ($dinoType as $type) {
-                    ?><option value="<?= $type ?>"><?= $type ?></option><?php } ?>
+                        foreach ($dinoType as $type) {
+                    ?><option value="<?= $type ?>" <?= isset($_POST['diet']) ? ($_POST['diet'] == $type ? 'selected' : '') : '' ?>><?= $type ?></option><?php } ?>
                 </select>
+                <?php if (isset($formErrors['diet'])) { ?>
+                        <p class="text-danger text-center"><?= $formErrors['diet'] ?></p>
+                <?php } ?>
             </div>
-            <div class="form-group col">
+<!--  -->
+            <div class="form-group col <?= count($_POST) > 0 ? (isset($formErrors['discoverer']) ? 'has-danger' : 'has-success') : '' ?>">
                 <label for="discoverer">Paléonthologue  : </label>
-                <input type="text" class="form-control" maxlength="50" placeholder="Ex: Allan Grant" name="discoverer" id="discoverer" />
-                <!-- PASSER L'input en minuscules puis avant envoie dans le BDD passer avec une Majuscule au début de chaque mot -->
+                <input type="text" placeholder="Ex: Allan Grant" name="discoverer" id="discoverer" class="form-control <?= count($_POST) > 0 ? (isset($formErrors['discoverer']) ? 'is-invalid' : 'is-valid') : '' ?>" <?= isset($_POST['discoverer']) ? 'value="' . $_POST['discoverer'] . '"' : '' ?> />
+            <?php if (isset($formErrors['discoverer'])) { ?>
+                <p class="text-danger text-center"><?= $formErrors['discoverer'] ?></p>
+            <?php } ?>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group <?= count($_POST) > 0 ? (isset($formErrors['discoverer']) ? 'has-danger' : 'has-success') : '' ?>">
             <label for="description">Description : </label>
-            <textarea type="text" class="form-control col" rows="10" id="description" placeholder="Description de la créature"></textarea>
+            <textarea type="text"  rows="10" name="description" id="description" placeholder="Description de la créature" class="form-control col <?= count($_POST) > 0 ? (isset($formErrors['description']) ? 'is-invalid' : 'is-valid') : '' ?>"><?= isset($_POST['description']) ? $_POST['description'] : '' ?></textarea>
+            <?php if (isset($formErrors['description'])) { ?>
+                <p class="text-danger text-center"><?= $formErrors['description'] ?></p>
+            <?php } ?>
             <div class="row">
                 <div class="col-md-4">
                     <label for="source">Source : </label>
